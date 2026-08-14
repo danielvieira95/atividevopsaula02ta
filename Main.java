3757d7d5d95178a903d1c271c515bbdffa919cc0
danielvieira95 <engdanielvieira95@gmail.com>
2026-08-13 22:50:49 -0300

    Alteração da mensagem

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args){
         System.out.println("Olá, Devops");
         System.out.println("Pipeline executada com sucesso !");
+        System.out.println("Prof Daniel Vieira");
     }
     
 }
